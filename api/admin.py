22a578d99488ecf35be3bae74b0132972d4cfbcd
mykumykuni <mykumykuni@users.noreply.github.com>
2026-04-13 15:00:43 +0800

    adjust admin wiring

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 from .models import Camera, Incident, UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ("user", "role", "created_at")
+	search_fields = ("user__username", "user__email", "role")
+
+
+@admin.register(Camera)
+class CameraAdmin(admin.ModelAdmin):
+	list_display = ("camera_code", "name", "location", "status", "last_active")
+	list_filter = ("status", "location")
+	search_fields = ("camera_code", "name", "location")
